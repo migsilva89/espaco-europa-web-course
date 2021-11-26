73fe75fcf71d52b03c1f9648650af91482a5da4d
migsilva89 <migdrum@gmail.com>
2021-11-26 18:14:57 +0100

    footer effects css with shadow

--- Landing page/index.php
+++ Landing page/index.php
@@ -14,66 +14,63 @@
 <body>
 <div class="centered-content">
 </div>
-    <?php
+
+<?php
     include "carousel.php";
     ?>
-    <h1>Ines Couto</h1>
+    <h1>Inês Couto</h1>
     <p class="subtitle">Home made Macrame</p>
-</header>
 
 <center>
 
-<div class="container mx-auto">
-    <div class="row mx-auto">
+    <div class="container mx-auto">
+        <div class="row mx-auto">
 
-        <?php
-        //variavel para imagens
-        $products= [
-            "./Images/macrame.jpg",
-            "./Images/macrame2.jpg",
-            "./Images/macrame3.jpg",
-            "./Images/macrame4.jpg",
-            "./Images/card.jpg",
-            "./Images/card.jpg",  "./Images/macrame.jpg",
-            "./Images/macrame2.jpg",
-            "./Images/macrame3.jpg",
-            "./Images/macrame4.jpg",
-            "./Images/card.jpg",
-            "./Images/card.jpg",
-        ];
-
-        ?>
+            <?php
+                //variavel para imagens
+                $products= [
+                    "./Images/macrame.jpg",
+                    "./Images/macrame2.jpg",
+                    "./Images/macrame3.jpg",
+                    "./Images/macrame4.jpg",
+                    "./Images/card.jpg",
+                    "./Images/card.jpg",  "./Images/macrame.jpg",
+                    "./Images/macrame2.jpg",
+                    "./Images/macrame3.jpg",
+                    "./Images/macrame4.jpg",
+                    "./Images/card.jpg",
+                    "./Images/card.jpg",
+                ];
+            ?>
 <!--   loop para todas as imagens-->
-        <?php foreach ($products as $image){ ?>
-            <div class="col-md-4 col-lg-2 col-sm-1 col-xs-1 mx-center">
-                <div class="card" style="background-image: url('<?php echo $image; ?>'); background-size: cover; background-repeat: no-repeat">
-                <a href="<?php echo $image; ?>" class="glightbox" data-gallery="gallery1">
-                    <div class="eye">
-                        <img src="Images/icon-view.svg" alt="...">
+            <?php foreach ($products as $image){ ?>
+                <div class="col-md-4 col-lg-2 col-sm-1 col-xs-1 mx-center">
+                    <div class="card" style="background-image: url('<?php echo $image; ?>'); background-size: cover; background-repeat: no-repeat">
+                    <a href="<?php echo $image; ?>" class="glightbox" data-gallery="gallery1">
+                        <div class="eye">
+                            <img src="Images/icon-view.svg" alt="...">
+                        </div>
+                    </a>
                     </div>
-                </a>
                 </div>
-            </div>
-         <?php } ?>
+             <?php } ?>
+        </div>
     </div>
-</div>
+
 </center>
 
 <footer>
     <div class="d-grid gap-2 d-md-block">
-        <button class="btn btn-outline-secondary btn-lg btn-inst" type="button">Instagram</button>
+        <button class="btn btn-outline-secondary btn-lg" type="button">Instagram</button>
         <button class="btn btn-outline-secondary btn-lg" type="button">Linkedin</button>
         <button class="btn btn-outline-secondary btn-lg" type="button">Contact</button>
     </div>
 </footer>
 
-
-
 <!--light box libray-->
 <script type="text/javascript">
     const lightbox = GLightbox({ selector: '.glightbox' });
 </script>
-
 
 </body>
 
