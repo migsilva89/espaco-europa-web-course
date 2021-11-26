7878685ccf8982c674bae8ef439cb493e1752086
migsilva89 <migdrum@gmail.com>
2021-11-26 13:12:52 +0100

    For each loop; Added lightbox;

--- Landing page/index.php
+++ Landing page/index.php
@@ -5,8 +5,10 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<title>Ines Couto</title>
-    <link rel="stylesheet" href="style.css"
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
+    <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
+    <title>Ines Couto</title>
 </head>
 
 <body>
@@ -18,35 +20,53 @@
     <p class="subtitle">Home made Macrame</p>
 </header>
 
+
 <ul>
     <li>
-        <div class="card" style="width: 18rem;">
-            <img src="./Images/macrame2.jpg" class="card-img-top" alt="...">
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img src="./Images/macrame3.jpg" class="card-img-top" alt="...">
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img src="./Images/macrame4.jpg" class="card-img-top" alt="...">
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img src="./Images/card.jpg" class="card-img-top" alt="...">
-        </div>
+        <?php
+        //variavel para imagens
+        $products= [
+            "macrame.jpg",
+            "macrame2.jpg",
+            "macrame3.jpg",
+            "macrame4.jpg",
+            "card.jpg",
+        ];
+
+        ?>
+<!--   loop para todas as imagens-->
+        <?php foreach ($products as $image){ ?>
+            <div class="card" style="width: 18rem;">
+                <a href="./Images/<?php echo $image; ?>" class="glightbox" data-gallery="gallery1">
+                    <img src="./Images/<?php echo $image; ?>" class="card-img-top" alt="...">
+                    <div class="eye">
+                        <img src="Images/icon-view.svg" alt="...">
+                    </div>
+                </a>
+            </div>
+         <?php } ?>
+
+
     </li>
 </ul>
 
 
 
-<!--<footer classe="footer">-->
-<!--    <ul>-->
-<!--        <li class="linkedin-btn">-->
-<!--            <a class="btn btn-primary" role="button" class="fab fa-instagram" href="https://www.instagram.com">Linkedin</a>-->
-<!--        </li>-->
-<!--        <li class="instagram-btn">-->
-<!--            <a class="btn btn-primary" role="button" class="fab fa-instagram" href="https://www.instagram.com">Instagram</a>-->
-<!--        </li>-->
-<!--    </ul>-->
-<!--</footer>-->
+
+
+<!--<footer class="icons-btn">-->
+<!--   <a> <ion-icon href-->
+<!--</footer>="https://instagram.com" name="logo-instagram"></ion-icon> </a>-->
+<!--    <a> <ion-icon name="logo-linkedin"></ion-icon> </a>-->
+
+
+<!--ICONS LIBRARY-->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+<script type="text/javascript">
+    const lightbox = GLightbox({ selector: '.glightbox' });
+</script>
 
 </body>
 
