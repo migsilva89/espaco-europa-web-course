67b78435d9f6c45a1e263f85bfc95e1b640bde68
migsilva89 <migdrum@gmail.com>
2021-11-26 04:16:19 +0100

    Initial commit

---
+++ Landing page/carousel.php
@@ -0,0 +1,7 @@
+
+
+<div class="row">
+    <div class="col-md-12">
+        <center><img src="./Images/macrame.jpg" class="banner"  alt="FAIL"></center>
+    </div>
+</div>
---
+++ Landing page/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<title>Ines Couto</title>
+    <link rel="stylesheet" href="style.css"
+</head>
+
+<body>
+<header>
+    <?php
+    include "carousel.php";
+    ?>
+    <h1>Ines Couto</h1>
+    <p class="subtitle">Home made Macrame</p>
+</header>
+
+<ul>
+    <li>
+        <div class="card" style="width: 18rem;">
+            <img src="./Images/macrame2.jpg" class="card-img-top" alt="...">
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="./Images/macrame3.jpg" class="card-img-top" alt="...">
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="./Images/macrame4.jpg" class="card-img-top" alt="...">
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="./Images/card.jpg" class="card-img-top" alt="...">
+        </div>
+    </li>
+</ul>
+
+
+<!--    <a href="mailto:Inescouto72@gmail.com">Contacta-me</a>-->
+
+
+
+
+
+
+
+
+
+
+<!--Conteudo -->
+<!--<div>-->
+<!--    <h2>-->
+<!--        Qual é o teu tipo de livro preferido?-->
+<!--    </h2>-->
+<!--    <ul>-->
+<!--        <li>Romance</li>-->
+<!--        <li>Crime</li>-->
+<!--        <li>Aventura</li>-->
+<!--    </ul>-->
+<!--</div>-->
+<!---->
+<!--<footer>-->
+<!--     Links relacionados no botao-->
+<!--    <ul>-->
+<!--        <li>-->
+<!--            <a href="https://www.linkedin.com">Linkedin</a>-->
+<!--        </li>-->
+<!--        <li>-->
+<!--            <a href="https://www.instagram.com">Instagram</a>-->
+<!--        </li>-->
+<!--    </ul>-->
+<!--</footer>-->
+
+</body>
+
+</html>
